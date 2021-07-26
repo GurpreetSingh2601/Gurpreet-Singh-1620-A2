005cb57dc1ed82acba8bbadd6ff8d0466385085f
Gurpreet Singh <gurubahre2019@gmail.com>
2021-07-26 12:41:07 -0700

    Adding form template for entering title and items

---
+++ app/app.js
@@ -0,0 +1,41 @@
+function budgetCardTemp(){
+	const card=`
+	<div class="budget-card">
+		<form>
+			<label for="title">Title:</label><br>
+			<input type="text" name="title" id='title'><br>
+			<label for="item1">Item 1:</label><br>
+			<input type="text" name="item1" id='item1'><br>
+			<label for="item2">Item 2:</label><br>
+			<input type="text" name="item2" id='item2'><br>
+		</form>
+	`
+}
+
+function gettitle(){
+
+}
+
+function getitems(){
+
+}
+
+function gettotal(){
+
+}
+
+function getvals(){
+	const buton=document.querySelector('#new-budget')
+	buton.addEventListener('click', () => {
+	const div=document.querySelector('.div-two')
+	const title=getTitle()
+	const items=getitems()
+	const total=gettotal()
+	div.insertAdjacentHTML()
+	}
+}
+
+function cleanup(){
+	const budgetcard=document.querySelector('.budget-card')
+	budgetcard.remove()
+}
